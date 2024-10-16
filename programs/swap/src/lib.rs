40e11ae739a0f13dc85d52b607f139d7f7ed6b82
samad <imsamad00@gmail.com>
2024-10-17 00:26:10 +0530

    initilised

---
+++ programs/swap/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("2xSfSmcDi39CegZvhc5qzzb4M63BMF76fkBaN1GqhAsj");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
