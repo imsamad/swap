2eeb9e0da1c8e523b67fe69295e55104a81b1fc9
samad <imsamad00@gmail.com>
2024-10-17 15:02:01 +0530

    bit of baked

--- programs/swap/src/instructions/make_offer.rs
+++ programs/swap/src/instructions/make_offer.rs
@@ -1,15 +1,7 @@
-// use anchor_lang::prelude::*;
-// use anchor_spl::{
-//     associated_token::AssociatedToken, 
-//     token_interface::{Mint, TokenAccount, TokenInterface}
-// };
-
-// use crate::{ANCHOR_DISCRIMINATOR, OFFER_SEED, Offer, transfer_tokens};
-
 use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken, 
-    token::{Mint, TokenAccount, Token},
+    token_interface::{Mint, TokenAccount, TokenInterface}
 };
 
 use crate::{ANCHOR_DISCRIMINATOR, OFFER_SEED, Offer, transfer_tokens};
@@ -20,18 +12,19 @@ pub struct MakeOffer<'info> {
     #[account(mut)]
     pub maker: Signer<'info>,
 
-    #[account(mut)]
-    pub token_mint_a: Account<'info, Mint>,
+    #[account(mint::token_program = token_program)]
+    pub token_mint_a: InterfaceAccount<'info, Mint>,
 
-    #[account(mut)]
-    pub token_mint_b: Account<'info, Mint>,
+    #[account(mint::token_program = token_program)]
+    pub token_mint_b: InterfaceAccount<'info, Mint>,
 
     #[account( 
         mut,
         associated_token::mint = token_mint_a,
         associated_token::authority = maker,
+        associated_token::token_program = token_program,
     )]
-    pub maker_token_account_a: Account<'info, TokenAccount>,
+    pub maker_token_account_a: InterfaceAccount<'info, TokenAccount>,
 
     #[account(
         init,
@@ -47,56 +40,14 @@ pub struct MakeOffer<'info> {
         payer = maker,
         associated_token::mint = token_mint_a,
         associated_token::authority = offer,
+        associated_token::token_program = token_program,
     )]
-    pub vault: Account<'info, TokenAccount>,
+    pub vault: InterfaceAccount<'info, TokenAccount>,
 
     pub system_program: Program<'info, System>,
-    pub token_program: Program<'info, Token>,
+    pub token_program: Interface<'info, TokenInterface>,
     pub associated_token_program: Program<'info, AssociatedToken>,
 }
-
-// #[derive(Accounts)]
-// #[instruction(id: u64)]
-// pub struct MakeOffer<'info> {
-//     #[account(mut)]
-//     pub maker: Signer<'info>,
-
-//     #[account(mint::token_program = token_program)]
-//     pub token_mint_a: InterfaceAccount<'info, Mint>,
-
-//     #[account(mint::token_program = token_program)]
-//     pub token_mint_b: InterfaceAccount<'info, Mint>,
-
-//     #[account( 
-//         mut,
-//         associated_token::mint = token_mint_a,
-//         associated_token::authority = maker,
-//         associated_token::token_program = token_program,
-//     )]
-//     pub maker_token_account_a: InterfaceAccount<'info, TokenAccount>,
-
-//     #[account(
-//         init,
-//         payer = maker,
-//         space = ANCHOR_DISCRIMINATOR + Offer::INIT_SPACE,
-//         seeds = [OFFER_SEED.as_bytes(), maker.key().as_ref(), id.to_le_bytes().as_ref()],
-//         bump
-//     )]
-//     pub offer: Account<'info, Offer>,
-
-//    #[account(
-//         init,
-//         payer = maker,
-//         associated_token::mint = token_mint_a,
-//         associated_token::authority = offer,
-//         associated_token::token_program = token_program,
-//     )]
-//     pub vault: InterfaceAccount<'info, TokenAccount>,
-
-//     pub system_program: Program<'info, System>,
-//     pub token_program: Interface<'info, TokenInterface>,
-//     pub associated_token_program: Program<'info, AssociatedToken>,
-// }
 
 pub fn send_offered_tokens_to_valut(
     context: &Context<MakeOffer>,
